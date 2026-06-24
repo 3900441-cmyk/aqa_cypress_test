0cf2f009baae5546d54505048c2e2db14bbec53e
Alona Husak <3900441@gmail.com>
2026-06-25 01:43:33 +0300

    updtae

--- cypress/pageObjects/GaragePage.js
+++ cypress/pageObjects/GaragePage.js
@@ -8,14 +8,13 @@ class GaragePage {
   get addExpenseButton() { return cy.get('.car-item').first().find('.btn-success'); } 
 
   addCar(brand, model, mileage) {
-    this.addCarButton.click();
-    this.brandSelect.select(brand);
-    this.modelSelect.should('not.be.disabled');
-    
-    this.modelSelect.select(model);
-    this.mileageInput.type(mileage);
-    this.submitAddCarButton.click();
-  }
+  this.addCarButton.click();
+  this.brandSelect.select(brand);
+  this.modelSelect.should('not.be.disabled');
+  this.modelSelect.select(model);
+  this.mileageInput.type(mileage);
+  this.submitAddCarButton.click();
+}
 
   clickAddExpenseForFirstCar() {
     this.addExpenseButton.click({ force: true });
