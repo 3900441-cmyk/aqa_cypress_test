db37eac708d890aa46486031a52afc1e36201ff1
3900441-cmyk <3900441@gmail.com>
2026-06-25 02:24:08 +0300

    Update cypress_api.cy.js

--- cypress/e2e/cypress_api.cy.js
+++ cypress/e2e/cypress_api.cy.js
@@ -24,6 +24,7 @@ describe('Car and Expenses Hybrid (UI + API) Flow', () => {
     
     cy.get('.btn-primary').contains('Add car').click();
     cy.get('#addCarBrand').select(testCar.brand);
+    
     cy.wait('@getModels'); 
     cy.get('#addCarModel').should('not.be.disabled').select(testCar.model);
     
